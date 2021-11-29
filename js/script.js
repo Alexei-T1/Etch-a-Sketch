b8fcb3e9ae503e0ae0fc7c5c6ed1d9665a33364c
Alexei-T1 <turencko.alexei@yandex.ru>
2021-11-29 12:35:04 +0400

    base place page-layout

--- js/script.js
+++ js/script.js
@@ -4,19 +4,19 @@ COUNT_V = 16;
 function creat(COUNT_H, COUNT_V) {
     let div_container = document.createElement("div");
 
-    div_container.classList.add("container");
+    div_container.classList.add("container_field");
 
     for (let i =  0; i < COUNT_V; i++){
         let div_container_row = document.createElement("div");
 
         div_container_row.classList.add("container_row");
-        div_container.appendChild(div_container_row);
+        div_container.append(div_container_row);
 
         for (let i =  0; i < COUNT_H; i++){
             let div_item = document.createElement("div")
 
             div_item.classList.add("item");
-            div_container_row.appendChild(div_item);   
+            div_container_row.append(div_item);   
         }       
     }
     return div_container;
@@ -24,4 +24,5 @@ function creat(COUNT_H, COUNT_V) {
 
 const div_container = creat(COUNT_H, COUNT_V);
 console.log(div_container);
-document.body.appendChild(div_container);
+const main = document.querySelector('.main');
+main.insertAdjacentElement("beforeend", div_container);
